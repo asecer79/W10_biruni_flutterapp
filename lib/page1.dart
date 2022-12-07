c462c63e8a6a56fc069c2071063df301ea1999b7
Aydin Secer <Aydin Secer@DESKTOP-7LBD65T>
2022-12-07 15:48:46 +0300

    u

--- lib/page1.dart
+++ lib/page1.dart
@@ -5,20 +5,27 @@ class Page1 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("Page1")),
-      body: Center(
-        child: Column(
-          children: [
-            TextField(),
-            ElevatedButton(
-              child: Text("Close"),
-              onPressed: () {
-                int age = 40;
-                Navigator.pop(context,age);
-              },
-            ),
-          ],
+    return WillPopScope(
+      onWillPop: () async {
+        return Future.value(false);
+      },
+      child: Scaffold(
+        appBar: AppBar(title: Text("Page1"),
+        automaticallyImplyLeading: false,
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              TextField(),
+              ElevatedButton(
+                child: Text("Close"),
+                onPressed: () {
+                  int age = 40;
+                  Navigator.pop(context, age);
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
